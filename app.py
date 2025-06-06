03cf9ff9f4795cbaeb9a4a29ecccb1f079a340b9
Monteiro <mail@pedromonteiro.uk>
2025-06-06 06:12:35 +0100

    Add files via upload

--- app.py
+++ app.py
@@ -16,7 +16,7 @@ if not st.session_state.acesso_autorizado:
     if st.button("Entrar"):
         if pin == PIN_CORRETO:
             st.session_state.acesso_autorizado = True
-            st.experimental_set_query_params(autorizado="1")
+            st.query_params["autorizado"] = "1"
             st.rerun()
         else:
             st.error("❌ Código incorreto. Tenta novamente.")
